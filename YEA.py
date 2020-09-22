cd8552038f144dd6695ba245a4c863dfcb106ef6
RoySegal <68278321+roysegal11@users.noreply.github.com>
2020-09-22 11:20:38 +0300

    Update YEA.py

--- YEA.py
+++ YEA.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     numb = int(sys.argv[1])+1
     for i in range(numb):
         if i != 0:
-            print(str(i)+" "+str(i**2))
+            print(str(i)+" "+str(i**3))
             time.sleep(1)
